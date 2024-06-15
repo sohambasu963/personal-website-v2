7d58773a160e2ab0e705837f2b9b58946f060dae
Soham Basu <sohambasu963@gmail.com>
2024-06-15 13:00:50 -0700

    added firebase analytics

---
+++ src/app/firebase/firebase.js
@@ -0,0 +1,33 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
+export const auth = getAuth(app);
+
+if (typeof window !== "undefined") {
+  isSupported().then((isSupported) => {
+    if (isSupported) {
+      const analytics = getAnalytics(app);
+    }
+  });
+}
+
+export const db = getFirestore(app);
